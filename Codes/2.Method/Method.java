92d5e80d4a717afbe90de6d38e96e1f434a45fa6
Akako Ziqi <69710421+akakoziqi@users.noreply.github.com>
2021-01-10 18:06:55 +0800

    Chapter 2 方法入门

---
+++ Codes/2.Method/Method.java
@@ -0,0 +1,42 @@
+public class Method {
+        public static void main(String[] args) {
+        // System.out.println("1");
+        // System.out.println("2");
+        // System.out.println("3");
+        // System.out.println("4");
+        print1();
+        
+        // System.out.println("5");
+        // System.out.println("6");
+        // System.out.println("7");
+        // System.out.println("8");
+        print2();
+
+        // System.out.println("9");
+        // System.out.println("10");
+        // System.out.println("11");
+        // System.out.println("12");
+        print3();
+    }
+
+    public static void print1() {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
+
+    public static void print2() {
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+    }
+
+    public static void print3() {
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+    }
+}
